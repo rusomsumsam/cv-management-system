81d6a98a7069dae896e938ced6d954bc27d8bb61
rusomsumsam <sbintemostofa@gmail.com>
2026-07-12 21:53:04 +0600

    feat: setup express backend foundation

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,4 +4,11 @@ router.get("/", (req, res) => {
     res.send("CV Management System API Running");
 });
 
+router.get("/health", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Server is healthy",
+    });
+});
+
 module.exports = router;
