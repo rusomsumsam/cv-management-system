2a60cc42ac9fc3d27a180d26f44f718fb86b38ba
rusomsumsam <sbintemostofa@gmail.com>
2026-07-18 05:08:28 +0600

    feat: add project model

---
+++ server/prisma/migrations/20260713134142_create_user_model/migration.sql
@@ -0,0 +1,21 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('CANDIDATE', 'RECRUITER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profilePhoto" TEXT,
+    "location" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'CANDIDATE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
