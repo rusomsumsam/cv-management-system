37f5dfdb841f9f3ea9362ff0b7d35365cfe951f4
rusomsumsam <sbintemostofa@gmail.com>
2026-07-18 05:29:09 +0600

    feat: add attribute model

---
+++ server/prisma/migrations/20260717231615_create_attribute_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Attribute" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attribute_pkey" PRIMARY KEY ("id")
+);
