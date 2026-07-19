86965362d3ab83b335017b009809af3de0bd0314
rusomsumsam <sbintemostofa@gmail.com>
2026-07-19 06:31:58 +0600

    feat: add login jwt middleware and role authorization

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -56,6 +56,86 @@ const registerUser = async (req, res) => {
     });
 };
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+    
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if (!isPasswordValid) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid credentials",
+        });
+    }
+
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+            role: user.role,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
+
+    return res.status(200).json({
+        success: true,
+        message: "Login successful",
+        token,
+        data: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            role: user.role,
+        },
+    });
+};
+
+const getCurrentUser = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.id,
+        },
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: {
+            id: user.id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            profilePhoto: user.profilePhoto,
+            location: user.location,
+            role: user.role,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+        },
+    });
+};
+
 module.exports = {
     registerUser,
+    loginUser,
+    getCurrentUser
 };
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,7 +1,48 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 
-const { registerUser } = require("../controllers/auth.controller");
+const {
+    registerUser,
+    loginUser,
+    getCurrentUser,
+} = require("../controllers/auth.controller");
+
+const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.get(
+    "/me",
+    authMiddleware,
+    getCurrentUser
+);
+
+const authorizeRoles = require("../middlewares/authorize.middleware");
+
+router.get(
+    "/admin",
+    authMiddleware,
+    authorizeRoles("ADMIN"),
+    (req, res) => {
+        res.status(200).json({
+            success: true,
+            message: "Welcome Admin",
+        });
+    }
+);
+
+router.get(
+    "/recruiter",
+    authMiddleware,
+    authorizeRoles("RECRUITER", "ADMIN"),
+    (req, res) => {
+        res.status(200).json({
+            success: true,
+            message: "Welcome Recruiter",
+        });
+    }
+);
 
 module.exports = router;
