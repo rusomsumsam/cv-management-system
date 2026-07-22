c21c4331aadce04bb9e6660938f4c459b6f87bce
rusomsumsam <sbintemostofa@gmail.com>
2026-07-23 04:55:16 +0600

    configure api url with environment variables

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://cv-management-system-gqwa.onrender.com/api",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
